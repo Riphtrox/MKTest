a3593433819e7c71772dbccb29068678916e75de
Riphtrox <Riphtrox@gmail.com>
2020-01-18 20:45:55 +1030

    Updated camera movement
    Camera now only follows the player horizontally

---
+++ MKTest/Assets/Scripts/CameraController.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+    public Transform cameraLoc;
+    public Transform playerLoc;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(cameraLoc.position.x != playerLoc.position.x)
+        {
+            cameraLoc.position = new Vector3(playerLoc.position.x, cameraLoc.position.y, cameraLoc.position.z);
+        }
+    }
+}
